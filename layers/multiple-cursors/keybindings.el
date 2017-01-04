57f2db3670d1dc2a3695b2ef75ac938e469761ff
deb0ch <thomas.de.beauchene@gmail.com>
2017-01-04 19:38:15 +0100

    move multiple-cursors to layers directory
    refactor in Spacemacs requires this change

--- layers/multiple-cursors/keybindings.el
+++ layers/multiple-cursors/keybindings.el
@@ -1,12 +1,11 @@
 
-(with-eval-after-load 'window-numbering
-
-  (define-key window-numbering-keymap (kbd "M-1") nil)
-  (define-key window-numbering-keymap (kbd "M-2") nil)
-  (define-key window-numbering-keymap (kbd "M-3") nil)
-  (define-key window-numbering-keymap (kbd "M-4") nil)
-  (define-key window-numbering-keymap (kbd "M-5") nil)
-  (define-key window-numbering-keymap (kbd "M-6") nil)
+(with-eval-after-load 'winum
+  (define-key winum-keymap (kbd "M-1") nil)
+  (define-key winum-keymap (kbd "M-2") nil)
+  (define-key winum-keymap (kbd "M-3") nil)
+  (define-key winum-keymap (kbd "M-4") nil)
+  (define-key winum-keymap (kbd "M-5") nil)
+  (define-key winum-keymap (kbd "M-6") nil)
 
   (global-set-key (kbd "M-1") 'mc/mark-all-like-this-dwim)
   (global-set-key (kbd "M-!") 'mc/mark-more-like-this-extended) ;; Todo: special actions transient state
