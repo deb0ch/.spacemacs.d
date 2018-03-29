5667726939a2aa5b03bec542d28181eb54a0588a
deb0ch <thomas.de.beauchene@gmail.com>
2018-03-29 14:30:13 +0200

    better highlight-delimiters setting

--- init.el
+++ init.el
@@ -416,7 +416,7 @@ It should only modify the values of Spacemacs settings."
    ;; Select a scope to highlight delimiters. Possible values are `any',
    ;; `current', `all' or `nil'. Default is `all' (highlight any scope and
    ;; emphasis the current one). (default 'all)
-   dotspacemacs-highlight-delimiters 'all
+   dotspacemacs-highlight-delimiters 'any
 
    ;; If non-nil, start an Emacs server if one is not already running.
    dotspacemacs-enable-server t
