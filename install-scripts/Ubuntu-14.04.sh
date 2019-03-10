fd95f9114e7414d67b57ee814bf6df8b8e2c007e
deb0ch <tom@blackfoot.io>
2019-03-10 13:54:34 +0100

    install-script: update install-commands and fix Ubuntu-14.04

--- install-scripts/Ubuntu-14.04.sh
+++ install-scripts/Ubuntu-14.04.sh
@@ -2,8 +2,8 @@
 # sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://deb0ch@github.com/deb0ch/spacemacs -b perso ~/.emacs.d
 git clone https://github.com/mirrors/emacs.git ~/emacs
-sudo apt-get build-dep emacs
-sudo apt install build-essential automake texinfo libjpeg-dev libncurses5-dev libtiff5-dev libgif-dev libpng-dev libxpm-dev libgtk-3-dev libgnutls28-dev
+sudo apt-get -y build-dep emacs
+sudo apt-get -y install build-essential automake texinfo libjpeg-dev libncurses5-dev libtiff5-dev libgif-dev libpng-dev libxpm-dev libgtk-3-dev libgnutls28-dev
 cd ~/emacs ; git checkout emacs-26.1.92
 cd ~/emacs ; ./autogen.sh
 cd ~/emacs ; ./configure
